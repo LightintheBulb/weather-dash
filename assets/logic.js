9b13f4b635dbd9be263f832c43a73d73aee840a2
Lightinthebulb <>
2022-10-25 22:36:02 -0400

    updated API Key with my own

--- assets/logic.js
+++ assets/logic.js
@@ -1,7 +1,7 @@
 // Global variables
 var searchHistory = [];
 var weatherApiRootUrl = 'https://api.openweathermap.org';
-var weatherApiKey = 'd91f911bcf2c0f925fb6535547a5ddc9';
+var weatherApiKey = '2c0ba1532fe0c57fbeab6142bc6ca380';
 
 // DOM element references
 var searchForm = document.querySelector('#search-form');
